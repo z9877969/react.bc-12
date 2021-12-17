d6c824ba6bce724851dfa4f93ff9d5410f9e3f3f
z9877969 <z9877969@ukr.net>
2021-12-17 14:10:27 +0200

    adds base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import { useEffect } from "react";
-import MainPage from "./components/_pages/MainPage/MainPage";
-import TransactionPage from "./components/_pages/TransactionPage/TransactionPage";
 import { useMainContext } from "./context/MainProvider";
 
 const App = () => {
   // const value = useContext(MainContext);
   const { activePage, error, setError } = useMainContext();
 
-  useEffect(() => {
-    if (error) {
-      alert(error);
-      setError(null);
-    }
-  }, [error]);
-
   return (
     <>
-      {activePage === "main" && <MainPage />}
-      {activePage === "costs" && <TransactionPage transType={"costs"} />}
-      {activePage === "incomes" && <TransactionPage transType={"incomes"} />}
-      {activePage === "balance" && <h1>Balance</h1>}
+      <h1>App</h1>
     </>
   );
 };
